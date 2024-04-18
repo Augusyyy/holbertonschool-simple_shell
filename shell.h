82b1951143319a57dee1ded6d44e095701d313ac
Augusyyy <8851@holbertonstudents.com>
2024-04-18 10:04:22 +1000

    newshellh

--- shell.h
+++ shell.h
@@ -1,12 +1,16 @@
 #ifndef SIMPLE_SHELL_SHELL_H
 #define SIMPLE_SHELL_SHELL_H
 
-#include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
 
 char *copy_line(void);
 char **cut_string(char *buffer);
